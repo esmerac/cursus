2e7b66b5aae74da511693461392514611abd253d
esmerac <151864945+esmerac@users.noreply.github.com>
2024-03-04 18:56:38 +0100

    Add files via upload

---
+++ libft/ft_itoa.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: emmera <emmera@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/14 17:48:36 by emmera            #+#    #+#             */
+/*   Updated: 2024/03/04 18:55:16 by emmera           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	len(long nb)
+{
+	int	len;
+
+	len = 0;
+	if (nb <= 0)
+	{
+		nb *= -1;
+		len++;
+	}
+	while (nb > 0)
+	{
+		nb /= 10;
+		len++;
+	}
+	return (len);
+}
+
+char	*ft_itoa(int nb)
+{
+	char	*str;
+	long	n;
+	int		i;
+
+	n = nb;
+	i = len (n);
+	str = (char *) malloc(i + 1);
+	if (!str)
+		return (NULL);
+	str[i--] = '\0';
+	if (n == 0)
+		str[0] = '0';
+	if (n < 0)
+	{
+		str[0] = '-';
+		n *= -1;
+	}
+	while (n > 0)
+	{
+		str[i] = '0' + (n % 10);
+		n /= 10;
+		i--;
+	}
+	return (str);
+}
