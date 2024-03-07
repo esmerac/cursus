2960882e9f4af1d075944c1d0f8457faad21ff11
esmerac <151864945+esmerac@users.noreply.github.com>
2024-03-07 16:08:28 +0100

    Update ft_lstdelone_bonus.c

--- libft/ft_lstdelone_bonus.c
+++ libft/ft_lstdelone_bonus.c
@@ -6,11 +6,16 @@
 /*   By: emmera <emmera@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 17:01:08 by emmera            #+#    #+#             */
-/*   Updated: 2024/03/04 17:01:28 by emmera           ###   ########.fr       */
+/*   Updated: 2024/03/07 15:56:22 by emmera           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*void ft_lstdelone(t_list *lst, void (*del)(void *))
+#include "libft.h"
+
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
 {
-	
-}*/
+	if (!lst || !del)
+		return ;
+	del (lst->content);
+	free (lst);
+}
