04e69420f35af29d5dc1e79bdbcce2bc4319b3b7
esmerac <151864945+esmerac@users.noreply.github.com>
2024-03-07 16:08:54 +0100

    Update ft_lstadd_front_bonus.c

--- libft/ft_lstadd_front_bonus.c
+++ libft/ft_lstadd_front_bonus.c
@@ -6,11 +6,16 @@
 /*   By: emmera <emmera@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 16:57:52 by emmera            #+#    #+#             */
-/*   Updated: 2024/03/04 16:58:09 by emmera           ###   ########.fr       */
+/*   Updated: 2024/03/06 17:58:12 by emmera           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*void ft_lstadd_front(t_list **lst, t_list *new)
+#include "libft.h"
+
+void	ft_lstadd_front(t_list **lst, t_list *new)
 {
-	
-}*/
+	if (!lst || !new)
+		return ;
+	new->next = *lst;
+	*lst = new;
+}
