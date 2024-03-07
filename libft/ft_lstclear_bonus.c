509d4a0666afb3be0ae1c9bccec7f595d8a981c5
esmerac <151864945+esmerac@users.noreply.github.com>
2024-03-07 16:02:38 +0100

    Update ft_lstclear_bonus.c

--- libft/ft_lstclear_bonus.c
+++ libft/ft_lstclear_bonus.c
@@ -6,11 +6,49 @@
 /*   By: emmera <emmera@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 18:13:24 by emmera            #+#    #+#             */
-/*   Updated: 2024/03/04 18:13:40 by emmera           ###   ########.fr       */
+/*   Updated: 2024/03/07 15:59:37 by emmera           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-/*void ft_lstclear(t_list **lst, void (*del)(void *))
+#include "libft.h"
+
+void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
-	
-}*/
+	t_list	*next;
+
+	while (*lst)
+	{
+		next = (*lst)->next;
+		ft_lstdelone(*lst, del);
+		*lst = next;
+	}
+	*lst = NULL;
+}
+/* void ft_free(void *content)
+{
+	content = NULL;
+}
+
+int	main(void)
+{
+	t_list	*lst;
+	t_list	*new;
+	t_list	*new2;
+	t_list	*new3;
+
+	lst = ft_lstnew("one");
+	new = ft_lstnew("two");
+	new2 = ft_lstnew("three");
+	new3 = ft_lstnew("four");
+	printf("%s\n", lst->content);
+	ft_lstadd_back(&lst, new);
+	printf("%s\n", ft_lstlast(lst)->content);
+	ft_lstadd_back(&lst, new2);
+	printf("%s\n", ft_lstlast(lst)->content);
+	ft_lstadd_back(&lst, new3);
+	printf("%s\n", ft_lstlast(lst)->content);
+	ft_lstclear(&lst, ft_free);
+	if (lst == NULL)
+		printf("la lista está vacía\n");
+	return (0);
+} */
