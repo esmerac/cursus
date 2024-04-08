8dace30ba2b66442b53c55a8d56b9990c51fdefa
esmerac <emily_mc@hotmail.com>
2024-04-08 18:58:39 +0200

    cambios

--- libftprintf/ft_printf.c
+++ libftprintf/ft_printf.c
@@ -6,9 +6,31 @@
 /*   By: emmera <emmera@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/02 20:05:08 by emmera            #+#    #+#             */
-/*   Updated: 2024/04/08 15:22:41 by emmera           ###   ########.fr       */
+/*   Updated: 2024/04/08 18:49:58 by emmera           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
+int	ft_putchar(char c)
+{
+	write (1, &c, 1);
+	return (1);
+}
+
+int	ft_type(va_list args, char str)
+{
+	int	i;
+
+	i = 0;
+	if (str == 'c')
+		return (ft_putchar(va_arg(args, int)));
+	else if (str == 's')
+		return (ft_putstr(va_arg(args, int)));
+	//else if (str == 'p')
+	//	return (ft_)
+	else if (str == 'd' || str == 'i')
+		return (ft_putnbr(va_arg(args, int)));
+	//else if (str == '')
+	return (0);
+}
